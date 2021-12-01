a03838dc4843b873d60e8a3cc9abbef17b1845f4
Ludvig <ludvig.fohns@gmail.com>
2021-12-01 09:13:01 +0100

    Initial commit

---
+++ src/main/java/friendrequestopgave/FriendrequestopgaveApplication.java
@@ -0,0 +1,13 @@
+package friendrequestopgave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendrequestopgaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendrequestopgaveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/friendrequestopgave/FriendrequestopgaveApplicationTests.java
@@ -0,0 +1,13 @@
+package friendrequestopgave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendrequestopgaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
